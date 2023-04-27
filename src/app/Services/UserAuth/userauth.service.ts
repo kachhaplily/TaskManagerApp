040c5b9dcc6a7db184a90ab3544a6aae014ae8cc
KACHHAP LILY <99547353+kachhaplily@users.noreply.github.com>
2023-04-27 16:45:58 +0530

    Forgot password update done!

--- src/app/Services/UserAuth/userauth.service.ts
+++ src/app/Services/UserAuth/userauth.service.ts
@@ -10,18 +10,25 @@ export class UserauthService {
   userid=localStorage.getItem("id");
   token=localStorage.getItem("token");
   constructor(private http: HttpClient) { }
+
+  // add data into database [post]
+
   postData({ data }: { data: any; }): Observable<any> {
     const headers = new HttpHeaders()
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json');
     return this.http.post<any>(this.apiurl ,data, { headers: headers });
   }
+  // login or authanticate
+
   login(data:any): Observable<any>{
     const headers = new HttpHeaders()
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json');
   return this.http.post<any>("https://localhost:7070/api/Auth/login" ,data, { headers: headers });
   }
+  // user update
+
   userUpdate(data:any): Observable<any> {
     const headers = new HttpHeaders()
       .set('Content-Type', 'application/json')
@@ -29,4 +36,7 @@ export class UserauthService {
       .set('Authorization', `bearer ${this.token}`);
     return this.http.put<any>(`https://localhost:7070/api/Auth/${this.userid}`,data, { headers: headers });
   }
+
+
+
 }
